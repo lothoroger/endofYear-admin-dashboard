f5130f311cdc7325251aafca6a522a2589f64480
lothoroger <lothor72@gmail.com>
2022-05-30 10:25:43 -0600

    corrected About component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgbPaginationModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
+
 export class AboutComponent implements OnInit {
 
   constructor() { }
